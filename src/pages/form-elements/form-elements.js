ed61e46c467fc0225b8ab2061c20c20c9fec8f05
Artemodium <getcss@yandex.ru>
2022-03-30 15:38:20 +0500

    Textfields, dropdowns, datefields, checkbox added

--- src/pages/form-elements/form-elements.js
+++ src/pages/form-elements/form-elements.js
@@ -1,1 +1,4 @@
-import '../../components/input/inputs.sass'
+import '../../components/fields/fields.sass'
+import '../../components/date-fields/date-fields.sass'
+import '../../pages/form-elements/form-elements.sass'
+import '../../components/checkbox/checkbox.sass'
