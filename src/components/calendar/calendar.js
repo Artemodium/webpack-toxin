122514558b63a5f2bb49a28b064e607aaf8dacdc
Artem <getcss@yandex.ru>
2022-06-19 19:59:23 +0500

    calendar edited

--- src/components/calendar/calendar.js
+++ src/components/calendar/calendar.js
@@ -1,16 +1,26 @@
 function Calendar(year, month) {
-    var Dlast = new Date(year, month + 1, 0).getDate(),
+    var Dlast = new Date(year, month, 0).getDate(),
         D = new Date(year, month, Dlast),
         DNlast = new Date(D.getFullYear(), D.getMonth(), Dlast).getDay(),
         DNfirst = new Date(D.getFullYear(), D.getMonth(), 1).getDay(),
-        calendar = $('.days__monthday'),
-        month = ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"];
-    currentMonth = [D.getMonth()]
+        month = ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"],
+        currentMonth = [D.getMonth()];
 
     $(document).ready(function() {
+        let currentMonth = new Date().getMonth()
+        let lastMonthLastDay = new Date(year, currentMonth, 0).getDate()
+        let nextMonthFirstDay = new Date(year, currentMonth + 2, 0).getDate()
+        let currentMonthLastDay = new Date(year, currentMonth - 1, 0).getDate()
+
+        console.log(currentMonth - 1, currentMonth, currentMonth + 1)
+        console.log(month[currentMonth], lastMonthLastDay, currentMonthLastDay, nextMonthFirstDay)
+
         $('.header__month').text(month[currentMonth])
-        $('.header__year').text(D.getFullYear())
-        let lastMonthLastDay = new Date(year, currentMonth - 2, 0).getDate()
+        $('.header__year').text(new Date().getFullYear())
+        Array.from($('.monthday-field').slice(DNfirst-1, DNfirst+Dlast-1)).map(elem => { $(elem).css({'color': 'red'})})
+        Array.from($('.monthday-field').slice(0, DNfirst-1)).map(elem => { $(elem).css({'color': 'blue'})})
+        Array.from($('.monthday-field').slice(DNfirst+Dlast-1, 42)).map(elem => { $(elem).css({'color': 'blue'})})
+        
         for (let i = DNfirst - 2; i >= 0; i--) {
             $('.monthday-field')[i].innerText = lastMonthLastDay--
         }
@@ -31,7 +41,20 @@ function Calendar(year, month) {
         D = new Date(year, currentMonth, Dlast)
         DNlast = new Date(D.getFullYear(), D.getMonth(), Dlast).getDay()
         DNfirst = new Date(D.getFullYear(), D.getMonth(), 1).getDay()
+        let lastMonthLastDay = new Date(year, currentMonth - 2, 0).getDate()
+        Array.from($('.monthday-field').slice(DNfirst-1, DNfirst+Dlast-1)).map(elem => { $(elem).css({'color': 'red'})})
+        Array.from($('.monthday-field').slice(0, DNfirst-1)).map(elem => { $(elem).css({'color': 'blue'})})
+        Array.from($('.monthday-field').slice(DNfirst+Dlast-1, 42)).map(elem => { $(elem).css({'color': 'blue'})})
         let count = DNfirst > 0 ? DNfirst - 1 : DNfirst
+        //console.log(currentMonth + ' month', DNfirst, Dlast)
+        //console.log(0, DNfirst-1)
+    
+        for (let i = DNfirst - 2; i >= 0; i--) {
+            $('.monthday-field')[i].innerText = lastMonthLastDay--
+        }
+        for (let i = DNfirst + Dlast - 1; i < 42; i++) {
+            $('.monthday-field')[i].innerText = i - 31
+        }
         for (let i = 1; i <= Dlast; i++) {
             $('.monthday-field')[count].innerText = i
                 ++count
