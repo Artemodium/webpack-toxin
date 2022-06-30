e3e13776f8438525edf849120afa509e7963f3de
Artemodium <getcss@yandex.ru>
2022-06-30 15:45:57 +0500

    headers-and-footers page started

---
+++ src/pages/headers-and-footers/headers-and-footers.js
@@ -0,0 +1,6 @@
+import './headers-and-footers.sass'
+import '../../components/header/header.sass'
+
+import '../../components/header/header.js'
+
+$(location).attr('pathname') == '/headers-and-footers.html' ? $('body').css('background-color', '#E5E5E5') : ''
